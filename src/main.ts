63c1414dcec5860bbffad05ce2c7b4056996d56e
CodeStar2244 <codestar2244@gmail.com>
2024-12-28 19:52:54 +0530

    [Add] : fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   console.log('Jay Swaminarayan... Shree Swaminarayan Vijayate...');
   console.log('Shree Ganeshaya Namah... Ganpati Bappa Moriya...');
   await app.listen(process.env.PORT ?? 3000);
