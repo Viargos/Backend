56c9e22e660734e54665b6125208598b78c46928
SarangTandel5112 <sarangtandel5112@gmail.com>
2024-12-28 19:55:36 +0530

    [Test] : git user name test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
   console.log('Jay Swaminarayan... Shree Swaminarayan Vijayate...');
   console.log('Shree Ganeshaya Namah... Ganpati Bappa Moriya...');
   await app.listen(process.env.PORT ?? 3000);
